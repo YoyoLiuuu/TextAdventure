dc7e370a5d9db07e3c5c0c65a01bd0cbf1866743
YoyoLiuuu <liuyimeng01@gmail.com>
2024-02-09 19:03:17 -0500

    game_data finish

--- game_data.py
+++ game_data.py
@@ -25,248 +25,244 @@ This file is Copyright (c) 2024 CSC111 Teaching Team
 """
 from typing import Optional, TextIO
 
+class Item:
+    """An item in our text adventure game world.
 
+
+    Instance Attributes:
+        - # TODO
+
+
+    Representation Invariants:
+        - # TODO
+    """
+
+    def __init__(self, name: str, start: int, target: int, target_points: int) -> None:
+        """Initialize a new item.
+        """
+
+        # NOTES:
+        # This is just a suggested starter class for Item.
+        # You may change these parameters and the data available for each Item object as you see fit.
+        # (The current parameters correspond to the example in the handout).
+        # Consider every method in this Item class as a "suggested method".
+        #
+        # The only thing you must NOT change is the name of this class: Item.
+        # All item objects in your game MUST be represented as an instance of this class.
+
+        self.name = name
+        self.start_position = start
+        self.target_position = target
+        self.target_points = target_points
+        self.current_location = start
 
 
 class Location:
-   """A location in our text adventure game world.
+    """A location in our text adventure game world.
 
 
-   Instance Attributes:
-       - # TODO
+    Instance Attributes:
+        - # TODO
 
 
-   Representation Invariants:
-       - # TODO
-   """
+    Representation Invariants:
+        - # TODO
+    """
+
+    def __init__(self, num: str, points: str, short: str, long: str, a_map: list[list[int]], items: list[Item]) -> None:
+        """Initialize a new location.
 
 
-   def __init__(self, location_num: str, points: str, short_description: str, long_description: str) -> None:
-       """Initialize a new location.
+        # TODO Add more details here about the initialization if needed
+        """
+        self.num = num
+        self.points = int(points)
+        self.short = short
+        self.long = long
+        self.actions = self.available_actions(int(num[-1]), a_map)
+        self.items = self.get_items(int(num[-1]), items)
+        self.visits = 0
+        self.map = map
 
+        # NOTES:
+        # Data that could be associated with each Location object:
+        # a position in the world map,
+        # a brief description,
+        # a long description,
+        # a list of available commands/directions to move,
+        # items that are available in the location,
+        # and whether the location has been visited before.
+        # Store these as you see fit, using appropriate data types.
+        #
+        # This is just a suggested starter class for Location.
+        # You may change/add parameters and the data available for each Location object as you see fit.
+        #
+        # The only thing you must NOT change is the name of this class: Location.
+        # All locations in your game MUST be represented as an instance of this class.
 
-       # TODO Add more details here about the initialization if needed
-       """
-       self.num = location_num
-       self.points = int(points)
-       self.short = short_description
-       self.long = long_description
-       self.actions = self.available_actions(int(location_num[-1]))
-       self.items =
+    def available_actions(self, location_num: int, game_map: list[list[int]]) -> list[str]:
+        """
+        Return the available actions in this location.
+        The actions should depend on the items available in the location
+        and the x,y position of this location on the world map.
+        """
+        for y in range(len(game_map)):
+            for x in range(len(game_map[0])):
+                if game_map[y][x] == location_num:
+                    loc_x = x
+                    loc_y = y
+        moves = []
+        if loc_y >= 1 and game_map[loc_y - 1][loc_x] != -1:
+            moves.append('N')
+        if loc_y <= len(game_map) - 2 and game_map[loc_y + 1][loc_x] != -1:
+            moves.append('S')
+        if loc_x >= 1 and game_map[loc_y][loc_x - 1] != -1:
+            moves.append('W')
+        if loc_x <= len(game_map) - 2 and game_map[loc_y][loc_x + 1] != -1:
+            moves.append('E')
+        return moves
 
-
-       # NOTES:
-       # Data that could be associated with each Location object:
-       # a position in the world map,
-       # a brief description,
-       # a long description,
-       # a list of available commands/directions to move,
-       # items that are available in the location,
-       # and whether the location has been visited before.
-       # Store these as you see fit, using appropriate data types.
-       #
-       # This is just a suggested starter class for Location.
-       # You may change/add parameters and the data available for each Location object as you see fit.
-       #
-       # The only thing you must NOT change is the name of this class: Location.
-       # All locations in your game MUST be represented as an instance of this class.
-
-
-       # TODO: Complete this method
-
-
-   def available_actions(self, location_num: int) -> list[str]:
-       """
-       Return the available actions in this location.
-       The actions should depend on the items available in the location
-       and the x,y position of this location on the world map.
-       """
-
-
-       # NOTE: This is just a suggested method
-       # i.e. You may remove/modify/rename this as you like, and complete the
-       # function header (e.g. add in parameters, complete the type contract) as needed
-
-
-       # TODO: Complete this method, if you'd like or remove/replace it if you're not using it
-
-
-
-
-class Item:
-   """An item in our text adventure game world.
-
-
-   Instance Attributes:
-       - # TODO
-
-
-   Representation Invariants:
-       - # TODO
-   """
-
-
-   def __init__(self, name: str, start: int, target: int, target_points: int) -> None:
-       """Initialize a new item.
-       """
-
-
-       # NOTES:
-       # This is just a suggested starter class for Item.
-       # You may change these parameters and the data available for each Item object as you see fit.
-       # (The current parameters correspond to the example in the handout).
-       # Consider every method in this Item class as a "suggested method".
-       #
-       # The only thing you must NOT change is the name of this class: Item.
-       # All item objects in your game MUST be represented as an instance of this class.
-
-
-       self.name = name
-       self.start_position = start
-       self.target_position = target
-       self.target_points = target_points
-
-
+    def get_items(self, location_num: int, items: list[Item]) -> list:
+        """docstring"""
+        items_at_location = []
+        for item in items:
+            if item.current_location == location_num:
+                items_at_location.append(item)
+        return items_at_location
 
 
 class Player:
-   """
-   A Player in the text advanture game.
+    """
+    A Player in the text advanture game.
 
 
-   Instance Attributes:
-       - # TODO
+    Instance Attributes:
+        - # TODO
 
 
-   Representation Invariants:
-       - # TODO
-   """
+    Representation Invariants:
+        - # TODO
+    """
 
+    def __init__(self, x: int, y: int) -> None:
+        """
+        Initializes a new Player at position (x, y).
+        """
 
-   def __init__(self, x: int, y: int) -> None:
-       """
-       Initializes a new Player at position (x, y).
-       """
+        # NOTES:
+        # This is a suggested starter class for Player.
+        # You may change these parameters and the data available for the Player object as you see fit.
 
-
-       # NOTES:
-       # This is a suggested starter class for Player.
-       # You may change these parameters and the data available for the Player object as you see fit.
-
-
-       self.x = x
-       self.y = y
-       self.inventory = []
-       self.victory = False
-
-
+        self.x = x
+        self.y = y
+        self.inventory = []
+        self.victory = False
 
 
 class World:
-   """A text adventure game world storing all location, item and map data.
+    """A text adventure game world storing all location, item and map data.
 
 
-   Instance Attributes:
-       - map: a nested list representation of this world's map
-       - # TODO add more instance attributes as needed; do NOT remove the map attribute
+    Instance Attributes:
+        - map: a nested list representation of this world's map
+        - # TODO add more instance attributes as needed; do NOT remove the map attribute
 
 
-   Representation Invariants:
-       - # TODO
-   """
+    Representation Invariants:
+        - # TODO
+    """
+
+    def __init__(self, map_data: TextIO, location_data: TextIO, items_data: TextIO) -> None:
+        """
+        Initialize a new World for a text adventure game, based on the data in the given open files.
 
 
-   def __init__(self, map_data: TextIO, location_data: TextIO, items_data: TextIO) -> None:
-       """
-       Initialize a new World for a text adventure game, based on the data in the given open files.
+        - location_data: name of text file containing location data (format left up to you)
+        - items_data: name of text file containing item data (format left up to you)
+        """
+
+        # NOTES:
+
+        # map_data should refer to an open text file containing map data in a grid format, with integers separated by a
+        # space, representing each location, as described in the project handout. Each integer represents a different
+        # location, and -1 represents an invalid, inaccessible space.
+
+        # You may ADD parameters/attributes/methods to this class as you see fit.
+        # BUT DO NOT RENAME OR REMOVE ANY EXISTING METHODS/ATTRIBUTES IN THIS CLASS
+
+        # The map MUST be stored in a nested list as described in the load_map() function's docstring below
+        self.map = self.load_map(map_data)
+        self.items = self.load_items(items_data)
+        self.location = self.load_location(location_data)
+
+        # NOTE: You may choose how to store location and item data; create your own World methods to handle these
+        # accordingly. The only requirements:
+        # 1. Make sure the Location class is used to represent each location.
+        # 2. Make sure the Item class is used to represent each item.
+
+    # NOTE: The method below is REQUIRED. Complete it exactly as specified.
+    def load_map(self, map_data: TextIO) -> list[list[int]]:
+        """
+        Store map from open file map_data as the map attribute of this object, as a nested list of integers like so:
 
 
-       - location_data: name of text file containing location data (format left up to you)
-       - items_data: name of text file containing item data (format left up to you)
-       """
+        If map_data is a file containing the following text:
+            1 2 5
+            3 -1 4
+        then load_map should assign this World object's map to be [[1, 2, 5], [3, -1, 4]].
 
 
-       # NOTES:
+        Return this list representation of the map.
+        """
+        map_raw = map_data.readlines()
+        the_map = []
+        for element in map_raw:
+            cur_list = []
+            raw_line = element.split()
+            for i in range(0, len(raw_line)):
+                cur_list.append(int(raw_line[i]))
+            the_map.append(cur_list)
+        return the_map
 
+    def load_location(self, location_data: TextIO) -> list[Location]:
+        """
+        COME BACK TO IT.
+        """
+        location_raw = location_data.readlines()
+        the_location = [[] for _ in range(40)]
+        cur_location = 0
+        for i in range(len(location_raw)):
+            location_raw[i] = location_raw[i][:-1]
+            if location_raw[i] == 'END':
+                cur_location += 1
+            else:
+                the_location[cur_location].append(location_raw[i])
+        final_location = []
+        for element in the_location:
+            while len(element) > 4:
+                element[-2] += element[-1]
+                element.pop()
+            final_location.append(Location(element[0], element[1], element[2], element[3], self.map, self.items))
+        return final_location
 
-       # map_data should refer to an open text file containing map data in a grid format, with integers separated by a
-       # space, representing each location, as described in the project handout. Each integer represents a different
-       # location, and -1 represents an invalid, inaccessible space.
+    def load_items(self, items_data: TextIO) -> list[Item]:
+        items_raw = items_data.readlines()
+        the_items = []
+        for element in items_raw:
+            item = element.split()
+            while len(item) > 4:
+                item[-2] = item[-2] + item[-1]
+                item.pop()
+            the_items.append(Item(item[3], int(item[0]), int(item[1]), int(item[2])))
+        return the_items
 
-
-       # You may ADD parameters/attributes/methods to this class as you see fit.
-       # BUT DO NOT RENAME OR REMOVE ANY EXISTING METHODS/ATTRIBUTES IN THIS CLASS
-
-
-       # The map MUST be stored in a nested list as described in the load_map() function's docstring below
-       self.map = self.load_map(map_data)
-       self.location = self.load_location(location_data)
-       self.items = self.load_items(items_data)
-
-
-       # NOTE: You may choose how to store location and item data; create your own World methods to handle these
-       # accordingly. The only requirements:
-       # 1. Make sure the Location class is used to represent each location.
-       # 2. Make sure the Item class is used to represent each item.
-
-
-   # NOTE: The method below is REQUIRED. Complete it exactly as specified.
-   def load_map(self, map_data: TextIO) -> list[list[int]]:
-       """
-       Store map from open file map_data as the map attribute of this object, as a nested list of integers like so:
-
-
-       If map_data is a file containing the following text:
-           1 2 5
-           3 -1 4
-       then load_map should assign this World object's map to be [[1, 2, 5], [3, -1, 4]].
-
-
-       Return this list representation of the map.
-       """
-       map_raw = map_data.readlines()
-       the_map = []
-       for element in map_raw:
-           cur_list = []
-           raw_line = element.split()
-           for i in range(0, len(raw_line)):
-               cur_list.append(int(raw_line[i]))
-           the_map.append(cur_list)
-       return the_map
-
-
-   def load_location(self, location_data: TextIO) -> list[Location]:
-       """
-       COME BACK TO IT.
-       """
-       location_raw = location_data.readlines()
-       the_location = [[] for _ in range(32)]
-       cur_location = 0
-       for element in location_raw:
-           if element == '\n':
-               cur_location += 1
-               pass
-           the_location[cur_location].append(element.rstrip())
-       final_location = []
-       for element in the_location:
-           final_location.append(Location(element[0], element[1], element[2], element[3]))
-       return final_location
-
-
-   def load_items(self, items_data: TextIO) -> list[list[str]]:
-
-
-
-
-
-
-   # NOTE: The method below is REQUIRED. Complete it exactly as specified.
-   def get_location(self, x: int, y: int) -> Optional[Location]:
-       """Return Location object associated with the coordinates (x, y) in the world map, if a valid location exists at
-        that position. Otherwise, return None. (Remember, locations represented by the number -1 on the map should
-        return None.)
-       """
-
-
-       # TODO: Complete this method as specified. Do not modify any of this function's specifications.
-
+    # NOTE: The method below is REQUIRED. Complete it exactly as specified.
+    def get_location(self, x: int, y: int) -> Optional[Location]:
+        """Return Location object associated with the coordinates (x, y) in the world map, if a valid location exists at
+         that position. Otherwise, return None. (Remember, locations represented by the number -1 on the map should
+         return None.)
+        """
+        if self.map[y][x] == -1:
+            return None
+        else:
+            return self.location[self.map[y][x]]
